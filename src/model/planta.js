2714ef0ae701517ef76d4264f8b924ee952d167e
explwi <andres.vera@tigo.net.py>
2025-05-06 12:51:02 -0300

    preparativos para agregar indexacion

--- src/model/planta.js
+++ src/model/planta.js
@@ -1,30 +1,54 @@
 const { DataTypes } = require('sequelize')
 const database = require('../database')
 
-const planta = database.define('planta',{
-    idplanta:{
-        primaryKey:true,
-        autoIncrement:true,
-        type:DataTypes.INTEGER
-    },
-    nombre:{
-        type:DataTypes.STRING,
-        allowNull:false
-    },
-    descripcion:{
-        type:DataTypes.STRING,
-        allowNull:false
-    },
-    estado:{
-        type:DataTypes.STRING,
-        allowNull:false
-    },
-    img:{
-        type:DataTypes.BLOB,
-    }
-},{
-    tableName:"planta",
-    timestamps:false
+const planta = database.define('planta', {
+  idplanta: {
+    primaryKey: true,
+    autoIncrement: true,
+    type: DataTypes.INTEGER
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  descripcion: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  estado: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  img: {
+    type: DataTypes.STRING
+  },
+  nombre_cientifico: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  familia: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  subfamilia: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  habitad_distribucion: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  ciclo_vida: {
+    type: DataTypes.STRING,
+    allowNull: true
+  },
+  fenologia: {
+    type: DataTypes.STRING,
+    allowNull: true
+  }
+}, {
+  tableName: 'planta',
+  timestamps: false
 })
 
-module.exports=planta
+module.exports = planta
